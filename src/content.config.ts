e3733d9b5e0d2e6c354edfa06dbbd8bf5e1e9111
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-02-21 12:52:43 +0700

    refactor(Welcome): simplify layout and remove unused assets; update footer styling
    feat(content): enhance content schema with optional fields for demo and source

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,23 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, z } from "astro:content";
 
-const content = defineCollection({
-    loader : glob({pattern : "**/*.{md,mdx}", base : "src/content"}),
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        date: z.coerce.date(),
-        demo: z.string(),
-        type: z.enum(["game", "blog", "website", "tool", "other"]),
-        stack: z.array(z.object({
-          name: z.string(),
-          link: z.string(),
-      }))
-        // pubDate: z.coerce.date(),
+const contentCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.coerce.date(),
+    demo: z.string().optional(), // opsional jika tidak ada demo
+    source: z.string().optional(), // opsional jika tidak ada source
+    type: z.enum(["game", "blog", "website", "tool", "other"]),
+    stack: z.array(
+      z.object({
+        name: z.string(),
+        link: z.string().url(),
       })
-})
+    ).optional(), // opsional jika tidak ada stack
+  }),
+});
 
-export const collections = { content };
+export const collections = {
+  content: contentCollection,
+};
