fd95ba993c428f24661b6f8704bb5fc510a34876
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-02-27 22:50:01 +0700

    chore: remove sitemap integration from configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react(),sitemap()],
-    site : 'https://alexanderar.my.id',
+  integrations: [mdx(), react()],
   vite: {
     plugins: [tailwindcss()]
   }
