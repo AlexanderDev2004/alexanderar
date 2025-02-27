197188e526b5487f715a190c4100da3bacf7a1f2
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-02-27 22:44:39 +0700

    feat: add sitemap integration and update index page to display games and website collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
-
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
-
+  integrations: [mdx(), react(),sitemap()],
+    site : 'https://alexanderar.my.id',
   vite: {
     plugins: [tailwindcss()]
   }
