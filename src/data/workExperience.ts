d9733a15a87ca5d03c891e3728312c6a71708318
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-03-09 00:55:22 +0700

    feat: update work experience entry for Ikatan Keluarga Banyuwangi with new name and detailed responsibilities

--- src/data/workExperience.ts
+++ src/data/workExperience.ts
@@ -23,7 +23,7 @@ export const workExperience = [
     },
     {
       company: {
-        name: "Ikatan Keluarga Banyuwangi (IKAWANGI)",
+        name: "Ikatan Keluarga Banyuwangi-Malang (IKAWANGI)",
         website: ""
       },
       position: "Human Resource Development",
@@ -31,15 +31,13 @@ export const workExperience = [
       time: "2023 - Present",
       period: [new Date(2023, 7), null],
       detail: [
-        "Support the Head of the Miniclass in planning and managing all activities.",
-        "Act as a liaison between the mentoring team and participants to ensure smooth communication.",
-        "Design and structure the syllabus, focusing on foundational game development concepts.",
-        "Ensure the curriculum aligns with industry trends and participant needs.",
-        "Provide hands-on mentoring to participants, offering guidance on technical and creative aspects of game development.",
-        "Gather feedback from participants and mentors to improve the miniclass experience.",
+        "Human Resource Management – Managing and developing organization members to work effectively and achieve optimal performance.",
+        "Training & Development – Organizing training programs, workshops, or mentoring sessions to enhance members' skills and capabilities.",
+        "Performance Management & Evaluation – Monitoring members' performance, providing feedback, and evaluating work effectiveness to achieve organizational goals."
+        
     ]
       ,
-      technologies: ["C#","Unity"]
+      technologies: ["NONE"]
     },
     
     
