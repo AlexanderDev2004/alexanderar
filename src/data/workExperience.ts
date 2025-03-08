48578b2d0b0dc21659d6662829cbb1dfc2d70eef
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-03-08 10:11:04 +0700

    feat: add WorkExperience component to display detailed work history and refactor Welcome component

---
+++ src/data/workExperience.ts
@@ -0,0 +1,27 @@
+import { date } from "astro:schema";
+
+export const workExperience = [
+    {
+      company: {
+        name: "Worksop Riset Informatika (WRI)",
+        website: "https://wridev.id/"
+      },
+      position: "Vice Head of Game Development divisional",
+      type: "hybrid",
+      time: "2025 - Present",
+      period: [new Date(2025, 2), null],
+      detail: [
+        "Support the Head of the Miniclass in planning and managing all activities.",
+        "Act as a liaison between the mentoring team and participants to ensure smooth communication.",
+        "Design and structure the syllabus, focusing on foundational game development concepts.",
+        "Ensure the curriculum aligns with industry trends and participant needs.",
+        "Provide hands-on mentoring to participants, offering guidance on technical and creative aspects of game development.",
+        "Gather feedback from participants and mentors to improve the miniclass experience.",
+    ]
+      ,
+      technologies: ["C#","Unity"]
+    },
+    
+  ];
+  
+  export default workExperience;  
