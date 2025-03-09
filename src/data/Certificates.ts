5c4f719748fdb15045031bb4b6db76fb90165753
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-03-09 13:16:34 +0700

    feat: add Certificates component and data for displaying user certificates

---
+++ src/data/Certificates.ts
@@ -0,0 +1,32 @@
+export const certificates = [
+  {
+    title: "Sql Basic Hackerrank Certificate",
+    description: "Sql Basic Hackerrank Certificate",
+    image: "/image/Certificates/Sql_Basic.png",
+    link: "",
+    tag : "Course",
+  },
+
+  {
+    title: "Mtcna Certificate",
+    description: "Certificate of Mikrotik Certified Network Associate",
+    image: "/image/Certificates/MTCNA_Mikrotik.png",
+    link: "",
+    tag : "Course",
+  },
+  {
+    title: "Dicoding Certificate Website basic",
+    description: "Dicoding Certificate Website basic",
+    image: "/image/Certificates/Dicoding_belajardasarweb.png",
+    link: "",
+    tag : "Course",
+  },
+  {
+    title: "Dicoding Certificate starter java",
+    description: "Dicoding Certificate starter java",
+    image: "/image/Certificates/Dicoding_memulaiJava.png",
+    link: "",
+    tag : "Course",
+  },
+
+];
