3afa64f88dc5d101f8907ec72ca4bc33698f4ba4
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-04-24 21:19:41 +0700

    feat: add markdown configuration with remark-gfm plugin and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,18 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+// import remarkToc from 'remark-toc';
+import remarkGfm from 'remark-gfm';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react()],
+  markdown: {
+    remarkPlugins: [ remarkGfm],
+    shikiConfig: {
+      theme: 'github-light'
+    }
+  },
   vite: {
     plugins: [tailwindcss()]
   }
